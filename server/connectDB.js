4b8531314cf616ae60cb6f57d47c00866b4a469c
kundalik jadhav <kundalikjadhav0807@gmail.com>
2024-10-23 20:15:00 -0700

    for test

--- server/connectDB.js
+++ server/connectDB.js
@@ -1,13 +1,6 @@
 import mongoose from "mongoose";
 
 const connectdb = async () => {
-  // try {
-  //   const mongoDBInstance = await mongoose.connect(process.env.MONGODB_URI);
-  //   console.log("🚀MongoDB connected successfully !!!");
-  // } catch (error) {
-  //   console.error("Mongodb connection failed!!!", error);
-  //   process.exit(1);
-  // }
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
