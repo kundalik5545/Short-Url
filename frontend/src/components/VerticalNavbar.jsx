9e191eed7d1eeeef08d8ce4923f168b52f0acb39
kundalik jadhav <kundalikjadhav0807@gmail.com>
2024-10-23 19:53:36 -0700

    corrected link import

--- frontend/src/components/VerticalNavbar.jsx
+++ frontend/src/components/VerticalNavbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, Link } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { home, link, chart, setting, add } from "../assets/icons/Icons";
 
 function VerticleNavbar() {
